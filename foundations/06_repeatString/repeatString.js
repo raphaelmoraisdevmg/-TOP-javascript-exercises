926767147da5634cb493d1a741c9f78bad0b5099
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-09 19:42:56 -0300

    developing a requirement
    making sure that negative numbers given as parameter will return "ERROR" as requiriment ask.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text,numberOfCopies) {
+  if(numberOfCopies < 0){
+    text = "ERROR"
+  }else{
+    for(let counter = 0;counter<numberOfCopies;counter++) {
+      text+=text;
+    }
+  }
+   
+   return text;
 };
-
+let copyString = repeatString("hey",-3)
+console.log(copyString);
 // Do not edit below this line
 module.exports = repeatString;
