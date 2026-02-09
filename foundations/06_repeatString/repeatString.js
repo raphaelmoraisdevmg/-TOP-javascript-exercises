b072194498ee304b39c39cc52377d7813677fffa
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-09 20:14:05 -0300

    making sure all exit cases are cover
    developing a if/elseif/else that  covers exit cases of function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,15 +1,12 @@
 const repeatString = function(text,numberOfCopies) {
   if(numberOfCopies < 0){
     text = "ERROR"
-  }else{
-    for(let counter = 0;counter<numberOfCopies;counter++) {
-      text+=text;
-    }
+  }else if(numberOfCopies == 0){
+    text ="";
+  } else{
+    text = text;
   }
-   
    return text;
 };
-let copyString = repeatString("hey",-3)
-console.log(copyString);
 // Do not edit below this line
 module.exports = repeatString;
