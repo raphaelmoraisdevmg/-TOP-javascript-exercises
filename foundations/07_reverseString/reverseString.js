1c568dd8ea707ad3ad529a8a38020d3122125a20
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-10 21:21:27 -0300

    partial solution for this challenge
    turn bigger problem into small ones in order to make easy to tackle the challenge

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,32 @@
-const reverseString = function() {
+function standardizingString(defaultText){
+  return defaultText.trim();
+}
+function extractfirstFragment(text){
+  return text.slice(0,text.length/2);
+}
+function extractSecondFragment(text){
+  return text.slice(text.length/2);
+}
+/*
+function reversingString(stringFragmentA,stringFragmentB,text){
+  let genString = "";
+  for(let extCount=0;extCount<text.length/2;extCount++){
+    for(let innerCount=text.length;innerCount>text.lenght/2;innerCount--){
 
-};
+    }
+  }
+}*/
+const reverseString = function(text) {
+  if(text=""){
+    return text
+  }else{
+    //const standardString=standardizingString(text);
+    //const firstFragment=extractfirstFragment(standardString);
+    //const secondFragment=extractSecondFragment(standardString);
+    //const reversedString=reversingString(firstFragment,secondFragment,standardString);
+    //return reversedString;
+}
+}
 
 // Do not edit below this line
 module.exports = reverseString;
