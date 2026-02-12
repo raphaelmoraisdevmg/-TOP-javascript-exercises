c6cbad8c52205af27606c276d08eafade2c4a553
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-11 21:44:14 -0300

    still in development

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -7,26 +7,36 @@ function extractfirstFragment(text){
 function extractSecondFragment(text){
   return text.slice(text.length/2);
 }
-/*
+
 function reversingString(stringFragmentA,stringFragmentB,text){
   let genString = "";
-  for(let extCount=0;extCount<text.length/2;extCount++){
-    for(let innerCount=text.length;innerCount>text.lenght/2;innerCount--){
-
+  // for(let extCount=0;extCount<text.length/2;extCount++){
+  //   genString+=text[extCount];
+  //   for(let innerCount=text.length;innerCount==text.lenght/2;innerCount--){
+      
+  //   }
+  // }
+  let auxA="",auxB="";
+  for(let count = 0; count == text.length;count++){
+    if(count<=text.length/2){
+      auxB+=text[count]
+    }else{
+      auxA+=text[count];
     }
   }
-}*/
+   genString = auxA + auxB;
+  return genString
+}
 const reverseString = function(text) {
   if(text==""){
     return text
   }else{
-    //const standardString=standardizingString(text);
-    //const firstFragment=extractfirstFragment(standardString);
-    //const secondFragment=extractSecondFragment(standardString);
-    //const reversedString=reversingString(firstFragment,secondFragment,standardString);
-    //return reversedString;
+    const standardString=standardizingString(text);
+    const firstFragment=extractfirstFragment(standardString);
+    const secondFragment=extractSecondFragment(standardString);
+    const reversedString=reversingString(firstFragment,secondFragment,standardString);
+    return reversedString;
 }
 }
-
 // Do not edit below this line
 module.exports = reverseString;
