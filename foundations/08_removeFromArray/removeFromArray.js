84ee1b19f98117b5934d182314edf18d026e1d4d
Raphael Almeida De Morais <raphaelmoraisdev@gmail.com>
2026-02-11 21:30:34 -0300

    my solution for exercise 7
    I developed my solution based in an if that ignores any menber of original array who is equal to  the number passed as parameter to my function and pushes to the result array all the others  menbers of original array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbersArray,item) {
+  let finalArray = [];
+  for(let count = 0;count<=numbersArray.length;count++){
+    if(numbersArray[count]==item){
+
+    }else{
+      finalArray.push(numbersArray[count]);
+    }
+  }
+  return finalArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
